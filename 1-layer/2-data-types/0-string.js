16ec582c6cfb7f8a315f58335db9f7dc4dc64471
Ratarca <ratarca.rtc@gmail.com>
2022-08-25 20:49:12 -0300

    more-files

---
+++ 1-layer/2-data-types/0-string.js
@@ -0,0 +1,24 @@
+/*
+Main Methods
+> length
+> indexOf
+> concat
+> replace
+> split
+> toLowerCase
+> trim
+> substr
+*/
+
+let s_string = "i'm a simple string";
+let obj_string = new String("Powerful_String")
+
+console.log(typeof s_string)
+console.log(typeof obj_string)
+
+//
+
+let size = s_string.length // Property doesnt have invoke
+
+console.log(size, idx_simple)
+
