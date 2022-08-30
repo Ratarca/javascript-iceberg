569c45b6fe9c0bf4edd0d3cbf638593a4644e027
Ratarca <ratarca.rtc@gmail.com>
2022-08-29 21:35:01 -0300

    structure:project

---
+++ iceberg/3-layer/1-data-structures/0-array/4.0-array.js
@@ -0,0 +1,13 @@
+/*
+ */
+
+let inflation = [1,2,3,4,5,'six']
+
+
+console.log(typeof inflation, '||' ,inflation,'\n')
+console.log(inflation[0], '||')
+
+
+// Matrix
+let matrix = [[1,2,3],[4,5,6]]
+console.log(matrix[0][1])
---
+++ iceberg/3-layer/1-data-structures/0-array/4.1-array-destructuring.js
@@ -0,0 +1,3 @@
+// Basic: Destructiring
+// Adv: Destructiring
+// Rest Operator
