5f73b75f78d481d06b6cfcaf66ad14569be22535
Ratarca <ratarca.rtc@gmail.com>
2022-09-08 22:55:57 -0300

    array-intro

---
+++ iceberg/3-layer/1-data-structures/0-array/4.0-unidimension-array.js
@@ -0,0 +1,31 @@
+/*
+Unidimension array
+  - Select values
+  - Modify
+  - Iteração
+  - Methods : {get size, add end, add beggin, remove end, remove first, find an idx, slice}
+
+  # Notes : objects using name to access values while array you need use index
+*/
+
+
+// Build array
+let inflation = [1,2,3,4,5,'six']
+
+console.log(typeof inflation, '||' ,inflation,'\n')
+inflation[0]= "fuji"
+console.log(inflation, '||')
+
+// add values (push and pop | )
+console.log('\n')
+inflation.push(101010) // add end
+inflation.pop() // remove and
+
+// Slice
+
+// indexOf , lastIndexOf
+
+// Splice
+
+
+
---
+++ iceberg/3-layer/1-data-structures/0-array/4.1-bidimensional-array.js
@@ -0,0 +1,7 @@
+
+// Matrix
+let matrix = [[1,2,3],[4,5,6]]
+console.log(matrix[0][1])
+
+// New array
+let my_array = new Array(1,2,3)
