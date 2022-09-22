c9eea9f0cbee71f450476922e6ea24edb59d0bfb
Ratarca <ratarca.rtc@gmail.com>
2022-09-22 19:27:09 -0300

    layer-1:fim

---
+++ iceberg/1-layer/5-files/1-csv_files.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+let ratarca = {
+  personal_name: "ratarca",
+  age          : 20,
+  income       : 500.75,
+  married      : false,
+  credit_score : 100
+}
+
+// fs.write(filepath,content,mode)
+
+// write csv
+// write json
---
+++ labs/00-theory_fixer/layer_1/1-layer_variables.js
@@ -0,0 +1,16 @@
+// Declare a variables about some 2 person
+let male_person     = "ratarca";
+let female_person   = "Bibi";
+let education_level = 10;
+let income          = 1000.75;
+
+let   couple = true;
+const happy  = true;
+
+console.log(`${male_person} and ${female_person} is a couple : ${couple}`)
+console.log(income)
+
+
+/*
+Comment with many lines to check
+*/
